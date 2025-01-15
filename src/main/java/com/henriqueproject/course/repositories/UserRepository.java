fd080442badccbd6c4ba396960936980f96d7568
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-15 18:11:28 -0300

    Service layer, component registration

--- src/main/java/com/henriqueproject/course/repositories/UserRepository.java
+++ src/main/java/com/henriqueproject/course/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.henriqueproject.course.repositories;
 import com.henriqueproject.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// não precisa colocar @Repository, pois está herdando do JpaRepository que
+// ja está registrado como um componente do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
---
+++ src/main/java/com/henriqueproject/course/services/UserService.java
@@ -0,0 +1,34 @@
+package com.henriqueproject.course.services;
+
+import com.henriqueproject.course.entities.User;
+import com.henriqueproject.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service // registra classe como componete do spring
+public class UserService {
+    // Nessa classe vai ter uma operação para buscar todos usuários e o usuário pelo Id dele
+
+    @Autowired // injeção de dependencia automatica de forma transapante ao programador
+
+    private UserRepository repository;
+
+
+    // Operação da camada do serviço findAll que repasse para camada de repository findAll
+    // e pega todos os usuários do banco
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj =  repository.findById(id);
+        return obj.get(); //Esse optional retorna com o get o objeto que está dentro desse
+        // optinal para apontarmos que o respository vai encontratar
+        //o Id do objeto user(da do pacote entites)
+    }
+
+}
