32f5307a5fd950490d2a44a3ae64c54b4ac30b24
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-15 16:31:42 -0300

    H2 database, test profile, JPA repository

--- src/main/java/com/henriqueproject/course/CourseApplication.java
+++ src/main/java/com/henriqueproject/course/CourseApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // Essas anotation @ é para indicar que é uma aplicação do springboot
-
 public class CourseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/henriqueproject/course/entities/User.java
+++ src/main/java/com/henriqueproject/course/entities/User.java
@@ -1,13 +1,19 @@
 package com.henriqueproject.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name= "tb_user")
 public class User implements Serializable {
 // Interface serializable é utiizado para que o objeto trafegue na rede, possa ser gravado em arquivo
     // pois tranforma o objeto em cadeias de byte
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
