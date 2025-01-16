ff273831a46d7f8922fee333137c699a8e0b923f
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-15 22:16:44 -0300

    Category entity

--- src/main/java/com/henriqueproject/course/config/TestConfig.java
+++ src/main/java/com/henriqueproject/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.henriqueproject.course.config;
 
+import com.henriqueproject.course.entities.Category;
 import com.henriqueproject.course.entities.Order;
 import com.henriqueproject.course.entities.User;
 import com.henriqueproject.course.entities.enums.OrderStatus;
+import com.henriqueproject.course.repositories.CategoryRepository;
 import com.henriqueproject.course.repositories.OrderRepository;
 import com.henriqueproject.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,21 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception { // tudo que tiver dentro desse método
         // vai ser executado quando a aplicação for iniciada
+
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Tauana Mireli", "tauana@gmail.com", "99943543", "23423432");
         User u2 = new User(null, "Henrique Silva", "henrique@gmail.com", "1231231231", "234234234");
     // id é nulo porque ele é implementado automaticamente pelo banco de dados
@@ -42,6 +55,9 @@ public class TestConfig implements CommandLineRunner {
         Order o1 = new Order(null, Instant.parse("2024-05-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2024-07-21T03:42:01Z"), OrderStatus.WAITING_PAYMENT,u2);
         Order o3 = new Order(null, Instant.parse("2024-08-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+
+
 
         //para salvar esses usuários acima no banco de dados:
         userRepository.saveAll(Arrays.asList(u1,u2)); // salvando usuários do objeto no banco
